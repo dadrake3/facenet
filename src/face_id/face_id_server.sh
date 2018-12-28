e788dc6c17e3ac7bf20585766d5051aae41248aa
Daryl Drake <dadrake3@gmail.com>
2018-12-28 15:40:27 -0600

    updated git ignore

--- src/face_id/face_id_server.sh
+++ src/face_id/face_id_server.sh
@@ -2,3 +2,4 @@
 
 /Users/daryldrake/daryldrake/bin/python -m Pyro4.naming &
 /Users/daryldrake/daryldrake/bin/python /Users/daryldrake/Desktop/CS_Projects/Active/face_id_facenet_fork/facenet/src/face_id_main.py --s
+
