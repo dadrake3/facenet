d8509adabb38ee921c66fa78c676a5001298f511
dadrake3 <dadrake3@illinois.edu>
2018-12-28 15:43:54 -0600

    remove

--- src/face_id/face_id_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/Users/daryldrake/daryldrake/bin/python -m Pyro4.naming &
-/Users/daryldrake/daryldrake/bin/python /Users/daryldrake/Desktop/CS_Projects/Active/face_id_facenet_fork/facenet/src/face_id_main.py --s
-
